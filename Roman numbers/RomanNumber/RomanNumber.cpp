5597213378538452086ffb4f83ea121d988debca
Trung Đức <100559245+ducvu25@users.noreply.github.com>
2023-05-19 10:27:39 +0700

    Update RomanNumber.cpp
    cập nhât thêm giá trị max

--- Roman numbers/RomanNumber/RomanNumber.cpp
+++ Roman numbers/RomanNumber/RomanNumber.cpp
@@ -14,20 +14,19 @@ bool RomanNumber::isValidDecimalNumber(string s) {
 }
 bool RomanNumber::isValidRomanNumber(string s) {
 	for (int i = 0; i < s.size(); i++)
-		if (!(s[i] == 'I' || s[i] == 'V' || s[i] == 'X' || s[i] == 'L' || s[i] == 'C' || s[i] == 'D'))
+		if (!(s[i] == 'I' || s[i] == 'V' || s[i] == 'X' || s[i] == 'L' || s[i] == 'C' || s[i] == 'D' || s[i] == 'M'))
 			return false;
 	return true;
 }
 string RomanNumber:: convertRomanToDecimal(string s) {
-	string ss = s;
 	int values[] = { 1000,  900,  500,  400,  100,   90,   50,   40,   10,    9,    5,    4,    1 };
 	string symbols[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 	int n = 0;
 	for (int i = 0; i < 13; i++) {
 		int value = 0;
-		while (ss.size() >= symbols[i].size() && ss.find(symbols[i]) == 0) {
+		while (s.size() >= symbols[i].size() && s.find(symbols[i]) == 0) {
 			value++;
-			ss.erase(0, symbols[i].size());
+			s.erase(0, symbols[i].size());
 		}
 		if (value != 0)
 			n += values[i]*value;
